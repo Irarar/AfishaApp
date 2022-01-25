d072588f7faf900e7be244d77920740591ad7e76
Evgeniy Senkevich <jeka2285311@mail.ru>
2022-01-25 17:45:47 +0300

    Add service&controller Afisha

---
+++ AfishaApp/Controllers/AfishaController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AfishaApp.Models;
+using AfishaApp.Services;
+
+namespace AfishaApp.Controllers
+{
+    public class AfishaController : Controller
+    {
+        private readonly IAfishaService _afishaService;
+
+        public AfishaController(IAfishaService afishaService)
+        {
+            _afishaService = afishaService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _afishaService.GetAllAfishasAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Afisha afisha)
+        {
+            await _afishaService.CreateAfishaAsync(afisha);
+            return RedirectToAction("Details");
+        }
+
+        public async Task<IActionResult> Edit(Guid afishaId)
+        {
+            return View(await _afishaService.GetAfishaAsync(afishaId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Afisha afisha)
+        {
+            var afishaId = await _afishaService.EditAfishaAsync(afisha);
+            return RedirectToAction("Details", new {afishaId});
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid afishaId)
+        {
+            await _afishaService.DeleteAfishaAsync(afishaId);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ AfishaApp/Services/IAfishaService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AfishaApp.Data;
+using AfishaApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AfishaApp.Services
+{
+    public interface IAfishaService
+    {
+        Task<IEnumerable<Afisha>> GetAllAfishasAsync();
+        Task<Afisha> GetAfishaAsync(Guid afishaId);
+    }
+
+    public class AfishaService : IAfishaService
+    {
+        private readonly AppDbContext _db;
+        public async Task<IEnumerable<Afisha>> GetAllAfishasAsync()
+        {
+            return await _db.Afishas
+                .Include(c => c.Category)
+                .ThenInclude(c => c.Country)
+                .ToListAsync();
+        }
+
+        public async Task<Afisha> GetAfishaAsync(Guid afishaId)
+        {
+            return await _db.Afishas
+                .Include(c => c.Category)
+                .FirstOrDefaultAsync(a => a.Id == afishaId);
+        }
+    }
+}
